09783f9cec30f2d7975d6102be08466ec7de9ec0
ferkev <ferkev@live.fr>
2018-07-09 18:45:46 +0200

    Movement and audio

--- Assets/rocket.cs
+++ Assets/rocket.cs
@@ -4,13 +4,69 @@ using UnityEngine;
 
 public class rocket : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    [SerializeField] float rcsThrust = 250f;
+    [SerializeField] float mainThrust = 5f;
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
+
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
 	
 	// Update is called once per frame
-	void Update () {
-        print("update");
-	}
+	void Update ()
+    {
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+        Thrust();
+        Rotate();
+
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+ 
+            if (!audioSource.isPlaying)
+            {
+                
+                audioSource.Play();
+            }
+
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true;
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.Q))
+
+        {
+            
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false; // resume control of the rotation
+    }
 }
